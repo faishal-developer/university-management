c98f393638c0c8647c51e953721a45cae85ed971
faishal-developer <foysal.professional@gmail.com>
2023-07-24 21:05:44 +0600

    testing

--- src/app/User/user.service.ts
+++ src/app/User/user.service.ts
@@ -1,3 +1,15 @@
-export const createUser = async () => {
-  // const createdUser = await
+import { IUser } from './user.interface'
+import { User } from './user.model'
+
+const createUser = async (user: IUser): Promise<IUser | null> => {
+  const createdUser = await User.create(user)
+
+  if (!createdUser) {
+    throw new Error('Failed to create User!')
+  }
+  return createdUser
+}
+
+export default {
+  createUser,
 }
