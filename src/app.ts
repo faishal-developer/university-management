be6452085f810ec4f539b063a580d82a88d7d72f
faishal-developer <foysal.professional@gmail.com>
2023-07-24 16:03:09 +0600

    check

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
-import express, { Application, Request, Response } from "express";
-import cors from 'cors';
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-const app:Application = express();
-const port =5005;
+const app: Application = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/",(req:Request,res:Response)=>{
-    res.send("testing purpose");
-});
-export default app;
+app.get('/', (req: Request, res:any) => {
+  res.send('testing purpose')
+})
+export default app
