5c481e948f8bc5621a594009ad671fcf8edc7fbf
faishal-developer <foysal.professional@gmail.com>
2023-08-17 23:31:02 +0600

    um-3

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -4,7 +4,7 @@ import { studentController } from './student.controller'
 import { StudentValidaion } from './student.validation'
 
 const router = express.Router()
-//working
+//working 2
 router.get('/:id', studentController.getSingleStudent)
 router.get('/', studentController.getAllStudents)
 router.delete('/:id', studentController.deleteStudent)
